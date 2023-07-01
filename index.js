da0e72d9d7c7067e9c8663e07bcc4d8ca437c4e7
shivampandey777 <126339561+shivampandey777@users.noreply.github.com>
2023-07-01 08:51:53 +0530

    commit  initial files

---
+++ index.js
@@ -0,0 +1,33 @@
+let h = document.getElementById("segment")
+let count = 0
+function add1(){
+    count = count + 1
+    h.innerText = count
+}
+function add2(){
+    count = count + 2
+    h.innerText = count
+}
+function add3(){
+     count = count +3
+     h.innerText = count
+}
+let o = document.getElementById("segment1")
+let count1 = 0
+function addOne(){
+    count1 = count1 + 1   
+    o.innerText = count1
+}
+function addTwo(){
+    count1 = count1 + 2 
+    o.innerText = count1
+}
+function addThree(){
+    count1 = count1 + 3 
+     o.innerText = count1
+}
+function re(){
+   o.innerText = count1 = 0 
+   h.innerText = count = 0     
+}
+
